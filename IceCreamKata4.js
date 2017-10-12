8a7b9c43b8e585d5f71eab49427257b808d9727c
MikePerezII <31927260+MikePerezII@users.noreply.github.com>
2017-10-12 08:33:41 -0400

    Add files via upload

---
+++ IceCreamKata4.js
@@ -0,0 +1,8 @@
+//Perform this operation until the variable is equal to a specific value
+do{
+	var flavor = prompt("What type of ice cream do you want? (chocolate, vanilla, rocky road, moose tracks, fudge, banana, or stawberry)");
+	if(flavor != "banana"){
+		alert("Try another flavor!")
+	}
+}
+while(flavor != "banana")
