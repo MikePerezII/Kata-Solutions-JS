e1009842cdbd0c221e5c351087da83c980affc6b
MikePerezII <31927260+MikePerezII@users.noreply.github.com>
2017-10-12 08:33:27 -0400

    Add files via upload

---
+++ IceCreamKata3.js
@@ -0,0 +1,11 @@
+//Perform this operation three times, using a for loop
+for(var index = 0; index < 3; index++){
+	var flavor = prompt("What type of ice cream do you want? (chocolate, vanilla, or stawberry)");
+	if (flavor == "chocolate" || flavor == "vanilla"){
+		alert("ye");
+	}else if(flavor == "strawberry"){
+		alert("nah");
+	}else{
+		alert("thats not a flavor");
+	}	
+}
