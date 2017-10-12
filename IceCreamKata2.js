8a22423c2940729643630242efef204a2482e032
MikePerezII <31927260+MikePerezII@users.noreply.github.com>
2017-10-12 08:33:10 -0400

    Add files via upload

---
+++ IceCreamKata2.js
@@ -0,0 +1,9 @@
+//Compare a value against three consitions, providing a unique response for each
+var flavor = prompt("What type of ice cream do you want? (chocolate, vanilla, or stawberry)");
+if (flavor == "chocolate" || flavor == "vanilla"){
+		alert("ye");
+	}else if(flavor == "strawberry"){
+		alert("nah");
+	}else{
+		alert("thats not a flavor");
+	}
